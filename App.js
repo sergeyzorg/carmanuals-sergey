c990d1b9c2c49328a3343504df9e50f6215519cd
sergeyzorg <zorgi@ukr.net>
2021-03-10 17:42:29 +0200

    Add image

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}
+      />
+      <Text style={styles.textmain}>Hello Car Manuals 3!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +22,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center',    
+  },
+  textmain: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'center',    
+    fontSize: 22,
+    fontWeight: "600",
+    color: "red"
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
   },
 });
